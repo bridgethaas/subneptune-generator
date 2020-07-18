34f64e629b90a374acf1c295a4f3ddae4f4b8404
bridgethaas <bridgethaas@Bridgets-MacBook-Pro.local>
2020-07-18 18:37:25 -0500

    add function for plotting all stages for a model

--- plotting.py
+++ plotting.py
@@ -1,66 +1,91 @@
+import os
 import numpy as np
-import matplotlib.pyplot as plt
 import mesa_reader as mr
+import matplotlib.pyplot as plt
+import matplotlib
+from scipy import interpolate
+from planets_grid.planets import *
 
-mearth = 5.97e27
-msun = 1.9892e33
-rearth = 6.371008e8
-rsun = 6.9598e10
-rfrac = rsun/rearth
-mfrac = msun/mearth
+matplotlib.rcParams['figure.facecolor'] = 'white'
 
-plt.figure()
-ax = plt.gca()
 
-def formatstring(myfloat):
-    return '%.5f'%myfloat
+def plot_early_stages(ax, planet, x, y, plot_evolve=True, **kwargs):
+    #ax - the axes you want to plot on
+    #planet - Planet class object we're plotting the data from
+    #x, y - quantities on the x and y axes 
 
-#mpList = [7.0,7.5,8.0]
-mpList = list(np.linspace(7.0,8.0,11))
-for i, m in enumerate(mpList):
-    mpList[i] = formatstring(m)
-#print(['%.5f'%item for item in mpList])
+    #make list containing the early stages for planet
+    ep = planet.get_early_stages()
 
-#fList = [0.009,0.01,0.011]
-fList = list(np.logspace(-4,np.log10(2*(10**-2)),10))
-for i, f in enumerate(fList):
-    fList[i] = formatstring(f) 
+    if plot_evolve == True:
+        ep.append(planet)
+        ax.set_xscale('log')
 
-entropyList = [7.28,7.28,7.29,7.29,7.3,7.3,7.3,7.31,7.31,7.32,7.32]
-for i, ent in enumerate(entropyList):
-    entropyList[i] = formatstring(ent)
+    #offset used when plotting vs timestep number
+    offset = 1
+    for stage in ep:
+        #TODO: handle when grid of early planets
+        if hasattr(stage, y):
+            ys = getattr(stage, y)[0]
+        else:   
+            ys = stage.datadicts[0][y]
 
-#styles = ['-','--','-.']
-alphas = [0.4,0.6,1]
-colors = ['r','b','g']
+        if x == 'timestep':
+            xs = np.arange(len(ys)) + offset
+            offset += len(xs)
+        else:
+            xs = getattr(stage, x)
 
-def envelope_fraction(history):
-    return history.envelope_mass/(history.star_mass*msun)
+        ax.plot(xs, ys, '.', label=stage.name, **kwargs)
+    
+        add_to_legend(ax, label=stage.name, c=stage.color) 
 
-def calcX(history):
-    return history.Hydrogen_Mass / history.envelope_mass
+    ax.set_xlabel(x)
+    ax.set_ylabel(y)
 
-def calcY(history):
-    return history.He4_Mass / history.envelope_mass
 
-def calcZ(history):
-    return 1 - calcX(history) - calcY(history)
+from matplotlib.lines import Line2D
+def add_to_legend(
+    ax,
+    label='',
+    shape='line',
+    loc=0,
+    legend_kwargs=None,
+    make_new_legend=False,
+    **kwargs):
 
-for i, m in enumerate(mpList):
-    ent = entropyList[i]
+    legend = ax.get_legend()
+    ## add the current legend to the tracked artists
+    ##  and then pretend we weren't passed one
+    if make_new_legend and legend is not None:
+        ax.add_artist(legend)
+        legend=None
 
-    for j, f in enumerate(fList):
-        h = mr.MesaData('data/biggrid/hist_evolve_%s_%s_0.24000_0.02000_0.03392_%s_0.10000.data'%(m,f,ent) ,file_type='log')
+    if legend is not None:
+        lines = legend.get_lines()
+        labels = [text.get_text() for text in legend.get_texts()]
+        if lines[0].get_linestyle() == 'None':
+            lines[0].set_marker('x')
+    else:
+        lines,labels=[],[]
 
-        plt.subplot(4, 3, i+1)
-        plt.plot(h.star_mass[-1]*mfrac,h.envelope_mass[-1]/(h.star_mass[-1]*msun),marker='.',color='b')
-        #ax.plot(h.star_age,envelope_fraction(h))
+    if legend_kwargs is None:
+        legend_kwargs = {}
 
-#ax.set_xlabel('time, Gyr')
-ax.set_ylabel('envelope fraction')
-#ax.set_yscale('log')
+    ## make the new line
+    if shape == 'line':
+        line = Line2D(
+        [0],[0],
+        **kwargs)
+    else:
+        raise NotImplementedError
 
-#lines = ax.get_lines()
-#ax.legend([lines[k] for k in [2,5,8]],['$M_0 = 7.0 M_E$','$M_0 = 7.5 M_E$','$M_0 = 8.0 M_E$']) 
+    if label not in labels:
+        lines.append(line)
+        labels.append(label)
 
-plt.show()
+    if loc in legend_kwargs:
+        loc = legend_kwargs.pop('loc')
+    ax.legend(lines,labels,loc=loc,**legend_kwargs)
+
+    return ax
